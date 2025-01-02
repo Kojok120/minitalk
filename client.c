d1b6cb16e22d207a8aadfa2fbb12c936bfd2c9be
岡本紘治 <>
2025-01-02 21:54:47 +0900

    途中で文字が消えるエラーを解消 mandatoryでfrancinette通る。

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: kokamoto <kokamoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 19:37:59 by kokamoto          #+#    #+#             */
-/*   Updated: 2024/12/29 22:20:03 by kokamoto         ###   ########.fr       */
+/*   Updated: 2025/01/02 21:44:57 by kokamoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,55 +14,33 @@
 #include <signal.h>
 #include <unistd.h>
 
-void send_char(pid_t pid, unsigned char *c, size_t len)
+static void	send_char(pid_t pid, unsigned char c)
 {
-    size_t byte_index;
-    int bit;
-    
-    byte_index = 0;
-    while (byte_index < len)
-    {
-        bit = 7;
-        while (bit >= 0)
-        {
-            if (c[byte_index] & (1 << bit))
-                kill(pid, SIGUSR1);
-            else
-                kill(pid, SIGUSR2);
-            usleep(1000);
-            bit--;
-        }
-        byte_index++;
-    }
+	int	bit;
+
+	bit = 7;
+	while (bit >= 0)
+	{
+		if (c & (1 << bit))
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		bit--;
+		usleep(200);
+	}
 }
 
-void send_str(pid_t pid, char *str)
+static void	send_str(pid_t pid, char *str)
 {
-    size_t i;
-    size_t len;
-    unsigned char *ustr;
+	int	i;
 
 	i = 0;
-	ustr = (unsigned char *)str;
-    while (ustr[i])
-    {
-        if ((ustr[i] & 0x80) == 0)
-            len = 1;
-        else if ((ustr[i] & 0xE0) == 0xC0)
-            len = 2;
-        else if ((ustr[i] & 0xF0) == 0xE0)
-            len = 3;
-        else if ((ustr[i] & 0xF8) == 0xF0)
-            len = 4;
-        else
-        {
-            i++;
-            continue;
-        }
-        send_char(pid, &ustr[i], len);
-        i += len;
-    }
-    send_char(pid, '\0', 1);
+	while (str[i])
+	{
+		send_char(pid, str[i]);
+		i++;
+	}
+	send_char(pid, '\0');
 }
 
 int	ft_atoi(const char *str)
@@ -99,11 +77,11 @@ int	main(int argc, char *argv[])
 	pid_t	pid;
 	char	*str;
 
-   if (argc != 3)
-   {
-       write(2, "Usage: ./client [server_pid] [string]\n", 37);
-       return (1);
-   }
+	if (argc != 3)
+	{
+		write(2, "Usage: ./client [server_pid] [string]\n", 37);
+		return (1);
+	}
 	pid = ft_atoi(argv[1]);
 	str = argv[2];
 	send_str(pid, str);
--- server.c
+++ server.c
@@ -6,112 +6,100 @@
 /*   By: kokamoto <kokamoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 19:38:11 by kokamoto          #+#    #+#             */
-/*   Updated: 2024/12/29 22:16:29 by kokamoto         ###   ########.fr       */
+/*   Updated: 2025/01/02 21:53:17 by kokamoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include <limits.h>
 #include <signal.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 1024
 
-static void sig_handler(int sig)
+void	*ft_memset(void *s, int c, size_t n)
 {
-    static int bit = 7;
-    static int byte_index = 0;
-    static unsigned char buf[BUFFER_SIZE] = {0};
-    static int utf8_bytes_remaining = 0;
+	unsigned char	*p;
+	size_t			i;
 
-    // ビットを設定
-    if (sig == SIGUSR1)
-        buf[byte_index] |= (1 << bit);
-    if (sig == SIGUSR2)
-        buf[byte_index] &= ~(1 << bit);
-    bit--;
-
-    // 1バイト完成
-    if (bit == -1)
-    {
-        bit = 7;
-
-        // 新しい文字の開始時にUTF-8バイト数を判定
-        if (utf8_bytes_remaining == 0)
-        {
-            if ((buf[byte_index] & 0x80) == 0)          // ASCII
-                utf8_bytes_remaining = 0;
-            else if ((buf[byte_index] & 0xE0) == 0xC0)  // 2バイト文字
-                utf8_bytes_remaining = 1;
-            else if ((buf[byte_index] & 0xF0) == 0xE0)  // 3バイト文字
-                utf8_bytes_remaining = 2;
-            else if ((buf[byte_index] & 0xF8) == 0xF0)  // 4バイト文字
-                utf8_bytes_remaining = 3;
-        }
-        else
-            utf8_bytes_remaining--;
-
-        byte_index++;
-
-        // バッファが一杯になるか、文字列終端に達したら出力
-        if (buf[byte_index - 1] == '\0' || byte_index >= BUFFER_SIZE)
-        {
-            write(1, buf, byte_index - 1);
-            memset(buf, 0, BUFFER_SIZE);
-            byte_index = 0;
-            utf8_bytes_remaining = 0;
-        }
-    }
+	p = s;
+	i = 0;
+	while (i < n)
+	{
+		p[i] = (unsigned char)c;
+		i++;
+	}
+	return (s);
 }
 
-
-void    ft_putint_fd(int n, int fd)
+static void	sig_handler(int sig)
 {
-    char    c;
+	static int				bit = 7;
+	static int				i = 0;
+	static unsigned char	buf[BUFFER_SIZE] = {0};
 
-    if (n == -2147483648)
-    {
-        write(fd, "-2147483648", 11);
-        return ;
-    }
-    if (n < 0)
-    {
-        n *= -1;
-        write(fd, "-", 1);
-    }
-    if (n < 10)
-    {
-        c = n + '0';
-        write(fd, &c, 1);
-        return ;
-    }
-    else
-    {
-        c = n % 10 + '0';
-        n = n / 10;
-        ft_putint_fd(n, fd);
-        write(fd, &c, 1);
-    }
+	if (sig == SIGUSR1)
+		buf[i] |= (1 << bit);
+	if (sig == SIGUSR2)
+		buf[i] &= ~(1 << bit);
+	bit--;
+	if (bit == -1)
+	{
+		bit = 7;
+		i++;
+		if (buf[i - 1] == '\0' || i >= BUFFER_SIZE)
+		{
+			write(1, buf, i);
+			ft_memset(buf, 0, BUFFER_SIZE);
+			i = 0;
+		}
+	}
 }
 
-int main(void)
+void	ft_putint_fd(int n, int fd)
 {
-    pid_t           pid;
-    struct sigaction    act;
+	char	c;
 
-    act.sa_handler = sig_handler;
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = 0;
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		n *= -1;
+		write(fd, "-", 1);
+	}
+	if (n < 10)
+	{
+		c = n + '0';
+		write(fd, &c, 1);
+		return ;
+	}
+	else
+	{
+		c = n % 10 + '0';
+		n = n / 10;
+		ft_putint_fd(n, fd);
+		write(fd, &c, 1);
+	}
+}
 
-    pid = getpid();
-    ft_putint_fd(pid, 1);
-    write(1, "\n", 1);
+int	main(void)
+{
+	pid_t				pid;
+	struct sigaction	act;
 
-    if (sigaction(SIGUSR1, &act, NULL) == -1 ||
-        sigaction(SIGUSR2, &act, NULL) == -1)
-        return (1);
-    while (1)
-        pause();
-    return (0);
+	act.sa_handler = sig_handler;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+	pid = getpid();
+	ft_putint_fd(pid, 1);
+	write(1, "\n", 1);
+	if (sigaction(SIGUSR1, &act, NULL) == -1 || sigaction(SIGUSR2, &act,
+			NULL) == -1)
+		return (1);
+	while (1)
+		pause();
+	return (0);
 }
